921f6b96c44136adbc0ce0715a836045dd40c572
mittwz <maarcus_usc@hotmail.com>
2018-02-05 04:05:12 -0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 # configure the dm text
 def generate_dm_text(name):
-	return '''Olá bb {} obrigado por me seguir, 
+	return '''Olá bb {} obrigado por me seguir, fique a vontade para enviar fotos, sigilo total, só posto se você quiser. 
 			Me adiciona no Snap? PutariaQuente 👅'''.format(name)
 
 scheduler_time = 15 #in minutes
 
 tw_username = "PutariaQuente" #change this to yours
-Oi linda, se quiser mandar fotos fique a vontade, sigilo total, só posto se você quiser!!!
-Add no snap: PutariaQuente -via @crowdfire
